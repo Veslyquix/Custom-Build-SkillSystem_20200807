6c9fbc759bdb994a9530e9392cbbec0a70436e68
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2020-11-16 16:46:51 -0500

    cmb artssdf

--- Text/Assemble Text.cmd
+++ Text/Assemble Text.cmd
@@ -2,7 +2,7 @@
 @echo off
 
 set "textprocess=%~dp0..\Tools\TextProcess\text-process-classic"
-set "parsefile=%~dp0..\Event Assembler\Tools\ParseFile.exe"
+set "parsefile=C:\Users\Vesly\Desktop\FEBuilderGBA\app\EA\Tools\ParseFile.exe"
 
 echo: | ("%textprocess%" "text_buildfile.txt" --parser-exe "%parsefile%")
 
